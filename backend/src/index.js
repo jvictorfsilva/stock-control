04189f17a757daac437e98c6276b4492d9ae9146
jvictorfsilva <jvictorfsilva@gmail.com>
2025-05-28 20:19:00 -0300

    fix: remove vercel and MySQL dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,7 @@
-import serverless from "serverless-http";
 import app from "./app.js";
 
-export default serverless(app);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
